fd483ead690ae8cfd56f664eaf50076cc51281e3
Carl Walsh <darthwalsh@gmail.com>
2016-02-22 08:48:23 -0800

    helloworld

---
+++ WebHost/HomePage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace WebHost
+{
+    public class HomePage : NancyModule
+    {
+        public HomePage()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
