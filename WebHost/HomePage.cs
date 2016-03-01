d96f37a0aa194c8d693036b115bd89a730e53af9
Carl Walsh <darthwalsh@gmail.com>
2016-03-01 02:33:40 -0800

    Refactor WebHost

--- WebHost/HomePage.cs
+++ WebHost/HomePage.cs
@@ -6,27 +6,10 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Nancy;
-using Nancy.Bootstrapper;
-using Nancy.Hosting.Aspnet;
 using Nancy.Responses;
-using Nancy.Responses.Negotiation;
 
 namespace WebHost
 {
-    //TODO refactor
-
-    public class CustomBootstrapper : DefaultNancyBootstrapper
-    {
-        protected override byte[] FavIcon => null;
-
-        protected override IRootPathProvider RootPathProvider => new AspNetRootPathProvider();
-
-        protected override NancyInternalConfiguration InternalConfiguration => NancyInternalConfiguration.WithOverrides(c =>
-        {
-            c.ResponseProcessors.Remove(typeof(JsonProcessor));
-        });
-    }
-
     public class HomePage : NancyModule
     {
         class Parsed
