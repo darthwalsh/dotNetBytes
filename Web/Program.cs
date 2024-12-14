4c108f9d02475a83aee91d0a664053d6b24904fc
Carl Walsh <darthwalsh@gmail.com>
2024-12-13 21:40:11 -0800

    Lambda is working

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.BadRequest("Only POST allowed"));
+
+app.MapPost("/", async (HttpContext context) =>
+{
+  using var buffer = new MemoryStream();
+  await context.Request.Body.CopyToAsync(buffer, 4096);
+  buffer.Position = 0;
+
+  var assm = new AssemblyBytes(buffer);
+
+  var options = context.RequestServices.GetService<IOptions<JsonOptions>>();
+  var json = assm.Node.ToJson(options?.Value.JsonSerializerOptions);
+  await context.Response.WriteAsync(json);
+});
 
 app.Run();
